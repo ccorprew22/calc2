131653515d9fdd46845dfc4e68acc435d5ae189e
Chris Corprew <ccorprew22@hotmail.com>
2021-11-11 14:05:25 -0500

    Linting main.py

--- main.py
+++ main.py
@@ -7,10 +7,3 @@ print("Welcome!")
 print("Type exit to end.")
 symbols = ["+","-","*","/"]
 calc = Calc()
-while True:
-    print("Enter another symbol and list of numbers.")
-    symbol = input("Symbol: ")
-    numbers = input("Numbers: ")
-    calc.get_symbol(symbol, numbers)
-    if calc.get_calculation_last() == "Error":
-        raise Exception("Can't divide by zero")
