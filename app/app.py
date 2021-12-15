e2a1055f3858c266bcbb585c0175f9eb24066727
Chris Corprew <ccorprew22@hotmail.com>
2021-12-15 18:17:42 -0500

    Update

--- app/app.py
+++ app/app.py
@@ -10,9 +10,9 @@ sys.path.append(parent_dir)
 #pylint: disable=wrong-import-position
 
 from app.controllers.index_controller import IndexController
-#from app.controllers.calculator_controller import CalculatorController
-#from app.controllers.results_table_controller import ResultsTableController
-#from app.controllers.most_recent_controller import MostRecentController
+from app.controllers.calculator_controller import CalculatorController
+from app.controllers.results_table_controller import ResultsTableController
+from app.controllers.most_recent_controller import MostRecentController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -22,21 +22,21 @@ def index():
     """index  Route Response"""
     return IndexController.get()
 
-# @app.route("/calculator", methods=['GET', 'POST'])
-# def basicform():
-#     """Basic Form Route"""
-#     if request.method == 'POST':
-#         return CalculatorController.post()
-#     return CalculatorController.get()
-#
-# @app.route("/results_table", methods=['GET', 'POST'])
-# def results_table():
-#     """ Results Table Route """
-#     if request.method == 'POST':
-#         return ResultsTableController.post()
-#     return ResultsTableController.get()
-#
-# @app.route("/most_recent")
-# def most_recent():
-#     """ Most recent result """
-#     return MostRecentController.get()
+@app.route("/calculator", methods=['GET', 'POST'])
+def basicform():
+    """Basic Form Route"""
+    if request.method == 'POST':
+        return CalculatorController.post()
+    return CalculatorController.get()
+
+@app.route("/results_table", methods=['GET', 'POST'])
+def results_table():
+    """ Results Table Route """
+    if request.method == 'POST':
+        return ResultsTableController.post()
+    return ResultsTableController.get()
+
+@app.route("/most_recent")
+def most_recent():
+    """ Most recent result """
+    return MostRecentController.get()
